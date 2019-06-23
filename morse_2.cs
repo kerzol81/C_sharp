3554b73f7220bc1e772db6b06376a0506a00d3f3
Z <kerenyi.nagy.zoltan@gmail.com>
2019-06-23 23:36:58 +0200

    Create morse_2.cs

---
+++ morse_2.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Text;
+using System.IO;
+
+namespace morse_2 {
+    class Program {
+        struct Morse {
+            public char letter;
+            public string word;
+            public string code;
+
+            public void DigestMorseCSV(string one_line) {
+                string[] csv_chunks = one_line.Split(',');
+                letter = Convert.ToChar(csv_chunks[0].ToLower());
+                word = Convert.ToString(csv_chunks[1]);
+                code = Convert.ToString(csv_chunks[2]);
+            }
+        }
+        static void Beep(string word) {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == '.')
+                {
+                    Console.Beep(440, 150);
+                }
+                else
+                    Console.Beep(440, 300);
+            }
+        }
+        static string Search(Morse[] morseTable, char c) {
+
+            int i = 0;
+            while (i < morseTable.Length && morseTable[i].letter != c)
+            {
+                i++;
+            }
+            if (i < morseTable.Length)
+            {
+                return morseTable[i].code;
+            }
+            return "";
+        }
+
+
+        static void Main(string[] args) {
+            StreamReader f = new StreamReader("morse.csv", Encoding.UTF8);
+            string[] lines = f.ReadToEnd().Split('\n');
+            Morse[] morseTable = new Morse[lines.Length - 1];
+
+            for (int i = 0; i < morseTable.Length; i++)
+            {
+                morseTable[i].DigestMorseCSV(lines[i]);
+            }
+
+            char c;
+            Console.WriteLine("* * * * M O R S E * * * * \t\texit: *");
+            do
+            {
+                c = Console.ReadKey().KeyChar;
+                Console.Write("|");
+                Beep(Search(morseTable, c));
+            } while (c != '*');
+
+            Console.ReadKey();
+        }
+    }
+}
