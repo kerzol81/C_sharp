6ccad42d2e3059c6a62c1523788231dafdc4f346
Z <kerenyi.nagy.zoltan@gmail.com>
2019-06-30 19:22:46 +0200

    Update sheep_hunter.cs

--- hw/sheep_hunter.cs
+++ hw/sheep_hunter.cs
@@ -6,50 +6,62 @@ using System.Threading.Tasks;
 
 namespace hunting {
     class Program {
+        static bool shoot = false;
         static void DisplayFence(char element)
         {
-            for (int i = 0; i < 21; i++)
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int y = 0; y < 21; y++)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.SetCursorPosition(0, i);
+
+                Console.SetCursorPosition(0, y);
                 Console.Write(element);
-                Console.SetCursorPosition(81, i);
+                Console.SetCursorPosition(81, y);
                 Console.Write(element);
             }
-            for (int i = 0; i < 81; i++)
+            for (int x = 0; x < 81; x++)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.SetCursorPosition(i, 0);
+                Console.SetCursorPosition(x, 0);
                 Console.Write(element);
-                Console.SetCursorPosition(i, 20);
+                Console.SetCursorPosition(x, 20);
                 Console.Write(element);
             }
         }
 
         static void PlaceSheeps(byte n)
         {
+            int x = 0;
+            int y = 0;
             Console.ForegroundColor = ConsoleColor.White;
             Random r = new Random();
             for (int i = 0; i < n; i++)
             {
-                Console.SetCursorPosition(r.Next(1, 79), r.Next(1, 19));
+                x = r.Next(1, 80);
+                y = r.Next(1, 20);
+                Console.SetCursorPosition(x, y);
                 Console.Write("x");
             }
+            // move sheeps when shooting happens
+        }
+
+        static void Shoot()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(0, 22);
+            Console.Write("Shoot:\nX: ");
+            byte x = Convert.ToByte(Console.ReadLine());
+            Console.Write("Y: ");
+            byte y = Convert.ToByte(Console.ReadLine());
+            Console.SetCursorPosition(x, y);
+            shoot = true;
         }
 
 
         static void Main(string[] args)
         {
             DisplayFence('#');
-            PlaceSheeps(6);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.SetCursorPosition(0,22);
-            Console.Write("Shoot:\nX: ");
-            byte x = Convert.ToByte(Console.ReadLine());
-            Console.Write("Y: ");
-            byte y = Convert.ToByte(Console.ReadLine());
-            Console.SetCursorPosition(x, y);
-
+            PlaceSheeps(1);
+            Shoot();
 
             Console.ReadKey();
         }
