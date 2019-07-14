f8682e8fea43f083f7ea5591bc047864fc45a863
Z <kerenyi.nagy.zoltan@gmail.com>
2019-07-14 13:53:22 +0200

    Add files via upload

---
+++ Practice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _03_namesAndGradesIntoFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter s = new StreamWriter("names.txt", true, Encoding.UTF8);
+            string name;
+            double average;
+
+            do
+            {
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                
+                
+                if (name != "exit")
+                {
+                    Console.Write("Average: ");
+                    average = Convert.ToDouble(Console.ReadLine());
+                    Student student = new Student(name, average);
+                    s.WriteLine(student.Name);
+                    s.WriteLine(student.Average);
+                    s.Flush();
+                }
+
+            } while (name != "exit");
+        }
+    }
+}
---
+++ Practice/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_namesAndGradesIntoFile
+{
+    class Student
+    {
+        
+
+        public string Name { get; set; }
+        public double Average { get; set; }
+
+        public Student(string name, double average)
+        {
+            Name = name;
+            Average = average;
+        }
+
+    }
+}
