f51a09a27bcf0fb51325913e275634405307d532
Z <kerenyi.nagy.zoltan@gmail.com>
2019-07-14 13:52:09 +0200

    Create Program.cs

---
+++ Practice/url_cleaner/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_urlNicer
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Url u = new Url("http://www.ádáméáí.hu");
+
+            Console.WriteLine(u.Nicer());
+                                  
+
+            Console.ReadKey();
+        }
+    }
+}
