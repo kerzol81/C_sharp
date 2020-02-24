af9f454628445e931e0b5197c20c10d4d2be9467
Z <kerenyi.nagy.zoltan@gmail.com>
2020-02-24 21:14:55 +0100

    Create only_numbers_in_textbox.cs

---
+++ Practice/only_numbers_in_textbox.cs
@@ -0,0 +1,7 @@
+private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
