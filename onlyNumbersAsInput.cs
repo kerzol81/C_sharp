99c7d3820cc02fb6a3da8e7a75087a9a96510383
Z <kerenyi.nagy.zoltan@gmail.com>
2020-03-30 19:02:04 +0200

    Update onlyNumbersAsInput.cs

--- onlyNumbersAsInput.cs
+++ onlyNumbersAsInput.cs
@@ -1,6 +1,6 @@
 private void egySzalAraTXTBX_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!(e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar == 8))   // the ASCII backspace is 8
+            if (0 < e.KeyChar && e.KeyChar <= 9 || e.KeyChar == 8)   // the ASCII backspace is 8
             {
                 e.Handled = true;
             }
