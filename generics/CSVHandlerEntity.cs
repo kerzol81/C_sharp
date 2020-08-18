565bb2cb6ed864a2b2df1e20216dc8f5df23dca7
Z <kerenyi.nagy.zoltan@gmail.com>
2020-08-18 17:15:11 +0200

    Create CSVHandlerEntity.cs

---
+++ generics/CSVHandlerEntity.cs
@@ -0,0 +1,13 @@
+static class CSVHandlerEntity<T> where T: ICSVFormat
+    {
+        public static void Write(List<T> items, string path)
+        {
+            StreamWriter s = new StreamWriter(path);
+            foreach (var item in items)
+            {
+                s.WriteLine(item.CSVFormat());
+            }
+            s.Close();
+        }
+        
+    }
