913adcf5cad3062b65ec8b5dba7564fac51f2c96
Z <kerenyi.nagy.zoltan@gmail.com>
2020-08-20 11:58:52 +0200

    Update GenericCSVHandler.cs

--- generics/GenericCSVHandler.cs
+++ generics/GenericCSVHandler.cs
@@ -1,4 +1,4 @@
-static class CSVHandlerEntity<T> where T : ICSVFormat, new()
+    static class CSVHandlerEntity<T> where T : ICSVFormat, new()
     {
         public static void Write(List<T> items, string path)
         {
@@ -16,33 +16,17 @@ static class CSVHandlerEntity<T> where T : ICSVFormat, new()
             foreach (var row in items)
             {
                 line = new StringBuilder();
-                foreach (var col in columns)
+                foreach (var column in columns)
                 {
-                    line.Append(col.GetValue(row));
+                    line.Append(column.GetValue(row));
                     line.Append(';');
                 }
                 csvlines.Add(line.ToString().Substring(0, line.Length - 1)); // 1 line, and ; removed from the end
             }
             File.WriteAllLines(path, csvlines);            
         }
-        //public static List<T> Read(string path)
-        //{
-        //    List<T> objects = new List<T>();
-        //    var lines = File.ReadAllLines(path).ToList();
-        //    var v = new T();
-        //    var attributes = v.GetType().GetProperties(); // reflexion
-        //    if (lines.Count > 0)
-        //    {
-        //        foreach (var row in lines)
-        //        {
-        //            v = new T();
-        //            var props = row.Split(';');
-        //            //         
-        //        }
-        //    }
-        //    else
-        //    {
-        //        throw new IndexOutOfRangeException("not rows in the file");
-        //    }
-        //}
+        public static List<T> Read(string path)
+        {
+            return null;
+        }
     }
